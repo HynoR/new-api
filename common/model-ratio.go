d831941a0f9392c35ef3bc8659da49f2ec37aeee
HynoR <20227709+HynoR@users.noreply.github.com>
2024-12-19 15:51:03 +0800

    refactor: 模型倍率分组

--- common/model-ratio.go
+++ common/model-ratio.go
@@ -59,6 +59,7 @@ var defaultCompletionRatio = map[string]float64{
 	"gpt-4o-gizmo-*": 3,
 	"gpt-4-all":      2,
 }
+var defaultModelRatio = initModelRatio()
 
 func GetModelPriceMap() map[string]float64 {
 	modelPriceMapMutex.Lock()
@@ -105,8 +106,8 @@ func GetModelPrice(name string, printErr bool) (float64, bool) {
 }
 
 func GetModelRatioMap() map[string]float64 {
-	modelRatioMapMutex.RLock()
-	defer modelRatioMapMutex.RUnlock()
+	modelRatioMapMutex.Lock()
+	defer modelRatioMapMutex.Unlock()
 	if modelRatioMap == nil {
 		modelRatioMap = defaultModelRatio
 	}
@@ -170,18 +171,80 @@ func UpdateCompletionRatioByJSONString(jsonStr string) error {
 	return json.Unmarshal([]byte(jsonStr), &CompletionRatio)
 }
 
-func GetCompletionRatio(name string) float64 {
-	// OpenAI Models
-	if strings.HasPrefix(name, "gpt") || strings.HasPrefix(name, "chatgpt") || strings.HasPrefix(name, "o1") {
-		return GetOpenAICompletionRatioData(name)
+var OpenAICompletionRatioDataDiff = map[string]float64{
+	"gpt-4o-2024-05-13":  3, // 第一版4o 为 3倍
+	"chatgpt-4o-latest":  3,
+	"gpt-3.5-turbo-0125": 3, // 最后一版3.5 turbo 为 3倍
+	"gpt-3.5-turbo-1106": 2, // 1106 为 2倍
+}
+
+func getOpenAICompletionRatioData(name string) float64 {
+	// 特殊倍率
+	if v, ok := OpenAICompletionRatioDataDiff[name]; ok {
+		return v
 	}
 
-	if strings.Contains(name, "claude-instant-1") {
-		return 3
-	} else if strings.Contains(name, "claude-2") {
-		return 3
-	} else if strings.Contains(name, "claude-3") {
+	if strings.HasPrefix(name, "gpt-4-gizmo") {
+		name = "gpt-4-gizmo-*"
+	}
+	if strings.HasPrefix(name, "gpt-4o-gizmo") {
+		name = "gpt-4o-gizmo-*"
+	}
+
+	// 逆向大手子
+	if strings.HasSuffix(name, "-all") || strings.HasSuffix(name, "-gizmo-*") {
+		return 1
+	}
+
+	// 4o 大家族基本上为4倍 , 05-13和chatgpt-4o-latest为3倍
+	if strings.HasPrefix(name, "gpt-4o") {
+		return 4
+	}
+
+	// o1
+	if strings.HasPrefix(name, "o1-") {
+		return 4
+	}
+
+	// gpt-4 / gpt-4-turbo 大家族基本上为2倍 带preview的为3倍, turbo为3倍
+	if strings.HasPrefix(name, "gpt-4") {
+		if strings.HasSuffix(name, "-preview") {
+			return 3
+		}
+		if strings.HasSuffix(name, "-turbo") {
+			return 3
+		}
+		return 2
+	}
+
+	// gpt-3.5 老版本均为1.33 (3/4)
+	if strings.HasPrefix(name, "gpt-3.5") {
+		return 3.0 / 4.0
+	}
+
+	// 默认倍率
+	return 1
+
+}
+
+func GetCompletionRatio(name string) float64 {
+	// Custom Completion Ratio
+	if ratio, ok := CompletionRatio[name]; ok && ratio > 0 {
+		return ratio
+	}
+
+	// OpenAI Models
+	if strings.HasPrefix(name, "gpt") || strings.HasPrefix(name, "chatgpt") || strings.HasPrefix(name, "o1") {
+		return getOpenAICompletionRatioData(name)
+	}
+
+	// Anthropic Models
+	if strings.Contains(name, "claude-3") {
 		return 5
+	}
+
+	if strings.Contains(name, "claude-2") || strings.Contains(name, "claude-instant-1") {
+		return 3
 	}
 
 	if strings.HasPrefix(name, "mistral-") {
@@ -192,7 +255,7 @@ func GetCompletionRatio(name string) float64 {
 	}
 	if strings.HasPrefix(name, "command") {
 		switch name {
-
+		// 淘汰模型暂不考虑倍率设置
 		case "command-r":
 			return 3
 		case "command-r-plus":
@@ -222,9 +285,7 @@ func GetCompletionRatio(name string) float64 {
 	case "llama3-70b-8192":
 		return 0.79 / 0.59
 	}
-	if ratio, ok := CompletionRatio[name]; ok {
-		return ratio
-	}
+
 	return 1
 }
 
